401db3e6cc3b11a4a581f158b42a19097c222a9e
MANUEL PANDELO <46505213@ort>
2022-06-22 10:15:12 -0300

    agregar nodemon y ultimo filtro

--- src/Services/ProfesorService.js
+++ src/Services/ProfesorService.js
@@ -6,22 +6,56 @@ import 'dotenv/config'
 const ProfesorTabla = process.env.DB_TABLA_Profesor;
 const PeticionTabla = process.env.DB_TABLA_Peticion;
 const ReviewTabla = process.env.DB_TABLA_Review;
+const IntermediaTabla= process.env.DB_TABLA_Intermedia
+const MateriaTabla= process.env.DB_TABLA_Materia
 
 export class ProfesorService {
 
-    getProfesor = async (materia, ubicacion, tipoclase,) => {
+    getProfesor = async (materia, ubicacion, tipo, activo) => {
         console.log('Get all Profesores by user preferences in Profesor Service');
         let response;
-        let query;
-        query=`SELECT * from ${ProfesorTabla}`;
-        response=await ProfesorHelper(undefined, query);
+        let query=`SELECT nombre, apellido, ubicacion, tipo from ${ProfesorTabla} inner join ${IntermediaTabla} on ${ProfesorTabla}.id = ${IntermediaTabla}.idProfesor inner join ${MateriaTabla} on ${IntermediaTabla}.idMateria=${MateriaTabla}.id  `; 
+        let where=false
+        let agregar=''
+
+        if(materia){
+            where = true;
+            agregar+=` Materia.nombre=@Materia`
+        }if(ubicacion){
+            if(where){
+                agregar+=` and ubicacion=@Ubicacion`;
+            }else{
+                where=true
+                agregar+=` ubicacion=@Ubicacion `
+            }
+        }if(tipo){
+            if(where){
+                agregar+=` and tipo=@TipoClase`;
+            }else{
+                where=true
+                agregar+=` tipo=@TipoClase `
+            }
+        }if(activo){
+            if(where){
+                agregar+=` and activo=@Activo`
+            }
+            else{
+                where=true
+                agregar+=` activo=@Activo `
+            }
+        }
+        if(where){
+            query+="WHERE " + agregar + "order by (Select AVG())"
+        }
+
+        response=await ProfesorHelper({materia, activo, ubicacion, tipo}, query);
         console.log(response)
         return response.recordset;
     }
 
     getProfesorById = async (id) => {
         console.log('Get Profesor by its ID in Profesor Service');
-        let query1=`SELECT * from ${ProfesorTabla} where id = @id`
+        let query1=`SELECT nombre, apellido, disponibilidad, ubicacion, tipo from ${ProfesorTabla} where id = @id`
         let query2=`SELECT * from ${ReviewTabla} where idProfesor = @id`
         let Profesor = ProfesorHelper({id}, query1);
         let Reviews =await ReviewHelper({id}, query2);
