2c6177c827744f0d1c0c23ef962d3a082cc05014
manupandelo <manuelpandelo12@gmail.com>
2022-08-10 22:27:30 -0300

    deberia funcionar

--- db.js
+++ db.js
@@ -2,17 +2,19 @@ import 'dotenv/config'
 import mysql from 'mysql2/promise'
 
 var connection = await mysql.createConnection({
-  host:'mysql.malamud.com.ar',
+  host: process.env.server,
+  user: process.env.user,
+  password: process.env.pass,
+  database: process.env.base
+  /*host:'mysql.malamud.com.ar',
   user:'tuprofesorback',
   password:'TuProfesorBackend!#',
-  database:'tuprofesorbackend'
+  database:'tuprofesorbackend'*/
 });
 
 connection.connect(function(err) {
   if (err) throw err;
   console.log("Database Connected!");
 });
-
-connection
 
 export default connection;
