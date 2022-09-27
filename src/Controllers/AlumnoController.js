700c01df8857c61a38c00de02728a378152da8c7
manupandelo <101258605+manupandelo@users.noreply.github.com>
2022-09-26 22:38:45 -0300

    Update AlumnoController.js

--- src/Controllers/AlumnoController.js
+++ src/Controllers/AlumnoController.js
@@ -40,7 +40,7 @@ router.get('/reviews/:id', /*Authenticate,*/ async (req, res) => {
 
 router.post('/register', /*Authenticate,*/ async (req, res) => {
   console.log(`This is a post operation`);
-  if(!req.body.nombre || !req.body.apellido || !req.body.ubicacion || !req.body.password || !req.body.email || req.body.telefono){
+  if(!req.body.nombre || !req.body.apellido || !req.body.ubicacion || !req.body.password || !req.body.email || !req.body.telefono){
     return res.status(400).json("Llenar todos los datos");
   }
   else{
