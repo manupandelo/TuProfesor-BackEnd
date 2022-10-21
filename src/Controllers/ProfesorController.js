dd6835603a535c338b399c3cdbfda54a5206aed6
Android Studio <devandroid@ort>
2022-10-21 08:22:47 -0300

    push materias agregado

--- src/Controllers/ProfesorController.js
+++ src/Controllers/ProfesorController.js
@@ -51,7 +51,7 @@ router.get('/peticion/:id', /*Authenticate,*/ async (req, res) => {
 
 router.post('/register', /*Authenticate,*/ async (req, res) => {
   console.log(`This is a post operation`);
-  if(!req.body.email || !req.body.password || !req.body.nombre || !req.body.apellido || !req.body.borndate || !req.body.ubicacion || !req.body.telefono || !req.body.disponibilidad || !req.body.tipo){
+  if(!req.body.email || !req.body.password || !req.body.nombre || !req.body.apellido || !req.body.borndate || !req.body.ubicacion || !req.body.telefono || !req.body.tipo){
     return res.status(400).json("Llenar todos los datos");
   }
   else{
